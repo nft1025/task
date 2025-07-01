097eeefdff4aeaaf18d1d5a8af63d943450e1258
nfteresa <teresa.neilfrancis@ue.edu.ph>
2025-07-01 11:01:56 +0800

    Boom panis

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Task Organizer',
+  description: 'created by team HALIBURTON',
+  generator: ':))',
 }
 
 export default function RootLayout({
